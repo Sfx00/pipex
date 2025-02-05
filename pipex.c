ea72c2d363e98ed72f872f94a2a0c6dbb54252f3
SFX <oussamaba348@gmail.com>
2025-02-06 00:37:35 +0100

    Add files via upload

---
+++ pipex.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obajali <obajali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 15:21:02 by obajali           #+#    #+#             */
+/*   Updated: 2025/02/06 00:22:54 by obajali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	execute_command(int input_fd, int output_fd, char *av, char **env)
+{
+	char	**cmd;
+	char	*path; 
+
+	if (dup2(input_fd, 0) == -1)
+		print_error(NULL, "Failed to dup input_fd",NULL, 1);
+	if (dup2(output_fd, 1) == -1)
+		print_error(NULL, "Failed to dup output_fd", NULL, 1);
+	cmd = ft_split(av, ' ');
+	if (!cmd || !*cmd)
+		print_error(NULL, "Command '' not found",cmd, 127);
+	path = find_path(cmd, env);
+	if (!path)
+		print_error(cmd[0], "Command not found",cmd, 127);
+	execve(path, cmd, env);
+	free(path);
+	print_error(cmd[0], "Command not found",cmd, 127);
+}
+
+void	second_child(t_rabat *card, char **av, char **env)
+{
+	open_file(av[4], 1, card);
+	close(card->pipe[1]);
+	execute_command(card->pipe[0], card->outfile, av[3], env);
+}
+
+void	first_child(t_rabat *card, char **av, char **env)
+{
+	open_file(av[1], 0, card);
+	close(card->pipe[0]);
+	execute_command(card->infile, card->pipe[1], av[2], env);
+}
+
+void	pipex(t_rabat *card, char **av, char **env)
+{
+	int	status1;
+	int	status2;
+
+	if (pipe(card->pipe) == -1)
+		print_error(NULL, "Failed to create pipe",NULL, 2);
+	card->pid1 = fork();
+	if (card->pid1 < 0)
+		print_error(NULL, "faild to fork",NULL, 254);
+	if (card->pid1 == 0)
+		first_child(card, av, env);
+	card->pid2 = fork();
+	if (card->pid2 < 0)
+		print_error(NULL, "faild to create a child",NULL, 254);
+	if (card->pid2 == 0)
+		second_child(card, av, env);
+	close(card->pipe[0]);
+	close(card->pipe[1]);
+	waitpid(card->pid1, &status1, 0);
+	waitpid(card->pid2, &status2, 0);
+	exit(WEXITSTATUS(status2));
+}
+
+int	main(int ac, char *av[], char *env[])
+{
+	t_rabat	card;
+
+	if (ac != 5)
+		print_error(NULL, "Usage: ./pipex file1 cmd1 cmd2 file2",NULL, 1);
+	if (!*env)
+		print_error(NULL, "Error empty Environment",NULL, 1);
+	pipex(&card, av, env);
+	return (0);
+}
---
+++ pipex_bonus.c
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obajali <obajali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 15:25:08 by obajali           #+#    #+#             */
+/*   Updated: 2025/02/06 00:34:41 by obajali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	open_files(char *file,t_rabat *card, int flag)
+{
+	int	fd;
+
+	if (flag == 0)
+	{
+		if (access(file, F_OK) == -1)
+		{
+			write_error(file, "No such file or directory\n");
+			return(create_pipe(card));
+		}
+		else if (access(file, R_OK) == -1)
+		{
+			write(2, "Permission denied\n", 18);
+			return(create_pipe(card));
+		}
+		fd = open(file, O_RDONLY);
+		
+	}
+	else
+	{
+		fd = check_file(file);
+		if (fd == -1)
+			print_error(NULL, "Permission denied",NULL, 1);
+	}
+	return (fd);
+}
+
+void	execute(char *argv, char **env)
+{
+	char	*path;
+	char	**cmd;
+
+	cmd = ft_split(argv, ' ');
+	if (!cmd)
+		print_error(NULL, "Failed to split command",NULL, 1);
+	path = find_path(cmd, env);
+	if (!path)
+		print_error(NULL, "Not found PATH of command",cmd, 127);
+	execve(path, cmd, env);
+	free(path);
+	print_error(NULL, "Command not found",cmd, 127);
+}
+
+void	pipex(char *cmd,t_rabat *card, char **env)
+{
+	int	pid;
+	
+	if (pipe(card->pipe) == -1)
+		print_error(NULL, "Failed to create pipe",NULL, 1);
+	pid = fork();
+	if (pid < 0)
+		print_error(NULL, "faild to fork",NULL, 1);
+	if (pid)
+	{
+		close(card->pipe[1]);
+		dup2(card->pipe[0], 0);
+	}
+	else
+	{
+		close(card->pipe[0]);
+		dup2(card->pipe[1], 1);
+		execute(cmd, env);
+	}
+}
+
+void	handle_here_doc(char *limiter)
+{
+	int		pipe_fd[2];
+	char	*line;
+
+	if (pipe(pipe_fd) == -1)
+		print_error(NULL, "Pipe error",NULL, 1);
+	while (1)
+	{
+		write(1, "heredoc> ", 9);
+		line = get_next_line(0);
+		if (!line)
+			break ;
+		if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0
+			&& line[ft_strlen(limiter)] == '\n')
+		{
+			free(line);
+			break ;
+		}
+		write(pipe_fd[1], line, ft_strlen(line));
+		free(line);
+	}
+	close(pipe_fd[1]);
+	dup2(pipe_fd[0], 0);
+	close(pipe_fd[0]);
+}
+
+void	wait_child(void)
+{
+	int state;
+	
+	while (wait(&state) != -1);
+	exit(state);
+}
+
+
+
+int	main(int ac, char **av, char **env)
+{
+	t_rabat	card;
+	int		i;
+
+	if (!env || ac < 5)
+		print_error(NULL, "Error: Invalid arguments",NULL, 1);
+	if (ft_strncmp(av[1], "here_doc", 8) == 0)
+	{
+		if (ac != 6)
+			print_error(NULL, "Usage: ./pipex here_doc LIMITER cmd1 cmd2 file",NULL,1);
+		handle_here_doc(av[2]);
+		card.outfile = open_files_doc(av[ac - 1]);
+		i = 3;
+	}
+	else
+	{
+		card.infile = open_files(av[1],&card, 0);
+		card.outfile = open_files(av[ac - 1], &card, 1);
+		dup2(card.infile, 0);
+		i = 2;
+	}
+	dup2(card.outfile, 1);
+	while (i < ac - 2)
+		pipex(av[i++],&card,env);
+	wait_child();
+}
